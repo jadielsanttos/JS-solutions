efb6ba398dd3a9e8877cbf01589a42f69d4bd19d
Jadiel Santos <82414367+jadielsanttos@users.noreply.github.com>
2022-02-21 22:04:15 -0300

    Delete js-logistica directory

--- js-logistica/layouts/footer.php
+++
@@ -1,7 +0,0 @@
-<head>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-
-<footer> 
-    &copy;created by - Jadiel Santos<br>
-</footer>
--- js-logistica/logout.php
+++
@@ -1,5 +0,0 @@
-<?php 
-    session_start();
-    unset($_SESSION['logado']);
-    header('location: login.php');
-?>
